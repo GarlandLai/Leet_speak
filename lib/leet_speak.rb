bed1af0438fc918eb4d9eb220f641508fbdec6df
Student <example@example.com>
2019-07-15 15:44:04 -0700

    add functional leet_speak with passing tests

--- lib/leet_speak.rb
+++ lib/leet_speak.rb
@@ -5,8 +5,14 @@ class String
     words = self.split(" ")
     words.each() do |word|
       word.length.times do |i|
-        if word[i] === "e"
+        if word[i] == "e"
           word[i] = "3"
+        elsif word[i] == "o"
+          word[i] = "0"
+        elsif word[i] == "I"
+          word[i] = "1"
+        elsif (word[i] == "s") && (i != 0)
+          word[i] = "z"
         end
       end
     end
